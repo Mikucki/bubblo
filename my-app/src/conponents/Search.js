1833a5f985785a91cce541e0a0fa64a1c14f691d
Mikucki <piotr.mikucki@op.pl>
2022-09-08 20:42:17 +0200

    added create react add

---
+++ my-app/src/conponents/Search.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+export default function Search() {
+  return (
+    <div>
+      <form>
+        <input type="search" placeholder="Search..." />
+      </form>
+    </div>
+  );
+}
